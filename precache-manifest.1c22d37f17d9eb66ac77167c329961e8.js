a9caa4a716db001e14a32d1206de55a3c0e0bf51
J. Sager Davidson <sager.davidson@gmail.com>
2019-05-04 10:03:13 -0500

    Updates

---
+++ precache-manifest.1c22d37f17d9eb66ac77167c329961e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d102db8f065957596713432233c0425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1aae92d10d4dff05dc6",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "fadbb0705bdd11d4fb8f",
+    "url": "/static/js/2.5d907561.chunk.js"
+  },
+  {
+    "revision": "f1aae92d10d4dff05dc6",
+    "url": "/static/js/main.3fcd5e50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
